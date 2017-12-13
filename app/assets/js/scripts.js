1f7d2fb016e99f007debedfb0072c6557a6f5cd1
David Simmer <david@simmerdesigns.com>
2017-12-13 10:25:54 -0600

    vanilla js DOM-ready method

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -404,3 +404,24 @@
  * Copyright 2017. CC BY-NC-ND licensed.
  */
 'use strict';
+
+function run() {
+  Barba.Pjax.start();
+}
+
+// in case the document is already rendered
+if (document.readyState!=='loading'){
+  run();
+}
+// modern browsers
+else if (document.addEventListener) {
+  document.addEventListener('DOMContentLoaded', run);
+}
+// IE <= 8
+else {
+  document.attachEvent('onreadystatechange', function() {
+    if (document.readyState==='complete') {
+      run();
+    }
+  });
+}
